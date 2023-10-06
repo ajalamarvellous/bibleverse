a6af71a3f66a6257a590f7a3ebafb9980830f20f
shelby <ajalaoluwamayowa00@gmail.com>
2023-10-06 20:43:24 +0100

    get embedding and bible map

---
+++ base.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+import pprint
+from io import BytesIO
+from urllib.request import urlopen
+from process import process
+from zipfile import ZipFile
+from gensim.models import KeyedVectors
+from gensim.scripts.glove2word2vec import glove2word2vec
+
+PDF_FILE = 'assets/Scripture_for_Every_Moment.pdf'
+URL = "http://nlp.stanford.edu/data/glove.6B.zip"
+embedding_filename = 'glove.6B.200d.txt'
+output_file = f"assets/{embedding_filename}.word2vec"
+
+def get_embedding_object():
+    if os.path.exists(output_file):
+        print("Loading GloVe embedding...")
+        glove = KeyedVectors.load_word2vec_format(output_file, binary=False)
+        return glove
+    
+    resp = urlopen(URL)
+    print("Downloading GloVe embedding...")
+    zipfile = ZipFile(BytesIO(resp.read()))
+    print("Opening GloVe embedding...")
+    zipfile.extract(embedding_filename)
+    print("Extracting GloVe embedding...")
+    glove2word2vec(embedding_filename, output_file)
+    print("Loading GloVe embedding...")
+    # shutil.move(embedding_filename, 'assets')
+    zipfile.close()
+    glove = KeyedVectors.load_word2vec_format(output_file, binary=False)
+    return glove
+
+# Path: home.py
+def verse_for_theme(feeling):
+    print("Welcome to the Bible Verse Finder")
+    bible_map = process(PDF_FILE)
+    print("Bible map created...")
+    print("One minute, creating embedding...")
+    embedding = get_embedding_object()
+    print("Embedding created...")
+    pprint.pprint(bible_map)
+
+if __name__ == '__main__':
+    main()
