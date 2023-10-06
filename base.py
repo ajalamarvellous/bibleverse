9e76bd66405da34088ead6b07a78bcac6c361e13
shelby <ajalaoluwamayowa00@gmail.com>
2023-10-06 13:44:08 +0100

    Add process_file to process the read file

--- base.py
+++ base.py
@@ -14,9 +14,26 @@ def read_asset(PDF_FILE):
         txt.append(pages.extract_text())
     return txt
 
+def process_text(txt):
+    """
+    Process the text and return the text in a list
+    """
+    whole_text = []
+    for page in txt:
+        page = page.split('\n \n')
+        for text in page[1:]:
+            text = text.split(' \n')
+            whole_text.extend([
+                x.strip(' ') for x in text if (
+                    isinstance(x, str) and x != ' '
+                    )
+                ])
+    return whole_text
+
 def main():
     file = read_asset(PDF_FILE)
-    print(file[0:5])
+    pages = process_text(file)
+    print(pages)
 
 if __name__ == '__main__':
     main()
